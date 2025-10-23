8ef398b14d970c4d1630363288f9df10c9d48a0a
Erix-glitch <71719838+Erix-glitch@users.noreply.github.com>
2025-10-22 20:00:08 -0700

    mitosis

---
+++ app/Word.js
@@ -0,0 +1,23 @@
+export async function Word() {
+    // build date string
+    const today = new Date();
+    const localTime = new Date(today.getTime() - today.getTimezoneOffset() * 60000);
+    const dateString = localTime.toISOString().split('T')[0]; // Local date in YYYY-MM-DD
+    // wordle api
+    const url = `https://www.nytimes.com/svc/wordle/v2/${dateString}.json`;
+
+    // fetch on server
+    let solution = null;
+    try {
+    const res = await fetch(url, { next: { revalidate: 86400 } }); // ISR: cache for 1 day
+    if (res.ok) {
+        const data = await res.json();
+        solution = data.solution.toUpperCase();
+    } else {
+        console.error('Wordle API returned', res.status);
+    }
+    } catch (error) {
+    console.error('Failed to fetch Wordle:', error);
+    }
+    return solution;
+}
--- app/page.js
+++ app/page.js
@@ -1,27 +1,9 @@
 // app/page.js
+import { Word } from './Word.js';
 export const revalidate = 86400; // revalidate every 24 hours
 
 export default async function Page() {
-  // build date string
-  const today = new Date();
-  const localTime = new Date(today.getTime() - today.getTimezoneOffset() * 60000);
-  const dateString = localTime.toISOString().split('T')[0]; // Local date in YYYY-MM-DD
-  // wordle api
-  const url = `https://www.nytimes.com/svc/wordle/v2/${dateString}.json`;
-
-  // fetch on server
-  let solution = null;
-  try {
-    const res = await fetch(url, { next: { revalidate: 86400 } }); // ISR: cache for 1 day
-    if (res.ok) {
-      const data = await res.json();
-      solution = data.solution.toUpperCase();
-    } else {
-      console.error('Wordle API returned', res.status);
-    }
-  } catch (error) {
-    console.error('Failed to fetch Wordle:', error);
-  }
+  const solution = await Word();
 
   // render html
   const letters = solution?.split('') ?? [];
