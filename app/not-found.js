2e82faef41c61d3fec617780e94e7e492a77899a
Erix-glitch <71719838+Erix-glitch@users.noreply.github.com>
2025-10-25 20:41:20 -0700

    add 404 cat

---
+++ app/not-found.js
@@ -0,0 +1,7 @@
+export default function NotFound() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-black">
+        <img src="https://http.cat/404"></img>
+    </div>
+  );
+}
