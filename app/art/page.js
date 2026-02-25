856858a497b0a620cb79fd4ebf5751e111f70c1b
Erix-glitch <71719838+Erix-glitch@users.noreply.github.com>
2026-02-25 13:26:14 -0800

    p

--- app/art/page.js
+++ app/art/page.js
@@ -1,11 +1,11 @@
 import { Nav } from '@/components/nav';
-export default async function Page({ searchParams }) {
+export default async function Page({ }) {
     return (
         <>
             <Nav />
             <main className="flex flex-col items-center justify-center min-h-screen text-auto text-center px-6 py-12">
                 <h1 className="text-4xl md:text-6xl font-bold tracking-tight drop-shadow-sm">
-                    Coming Soon
+                    Wordle Art Maker
                 </h1>
             </main>
         </>
